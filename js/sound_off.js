e17bbb1aa04fca937014a834bf554b958f0bafc3
YoshikiTaniguchi <84975821+kokonasa@users.noreply.github.com>
2021-12-23 18:02:45 +0900

    Add files via upload

---
+++ js/sound_off.js
@@ -0,0 +1,13 @@
+const sound_button = document.querySelector('.sound');
+const bgm = document.querySelector(".bgm");  
+
+sound_button.onclick = function(){
+    let mySrc = sound_button.getAttribute('src');
+    if(mySrc === 'assets/header_sound_off.png') {
+      sound_button.setAttribute('src','assets/header_sound.png');
+      bgm.play();
+    } else {
+      sound_button.setAttribute('src','assets/header_sound_off.png');
+      bgm.pause();
+    }
+}
